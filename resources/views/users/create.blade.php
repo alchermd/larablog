55f0ee23f242f35791f5c9631f1140bd60c30b6b
alchermd <johnalcherdoloiras@gmail.com>
2018-03-01 23:46:59 +0800

    Display old values when redirect back with input.

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -16,12 +16,12 @@
 
                     <div class="form-group">
                         <label for="username">Username</label>
-                        <input type="text" name="username" id="username" class="form-control" required>
+                        <input type="text" name="username" id="username" class="form-control" value="{{ old('username') }}" required>
                     </div>
 
                     <div class="form-group">
                         <label for="email">Email</label>
-                        <input type="email" name="email" id="email" class="form-control" required>
+                        <input type="email" name="email" id="email" class="form-control" value="{{ old('email') }}" required>
                     </div>
 
                     <div class="form-group">
