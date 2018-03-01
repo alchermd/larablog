2d150e214f3fbfefac85ca11307f102ef441c392
alchermd <johnalcherdoloiras@gmail.com>
2018-03-01 23:21:45 +0800

    Add route for viewing all posts.

--- routes/web.php
+++ routes/web.php
@@ -2,5 +2,7 @@
 
 Route::get('/', 'PagesController@home');
 
+Route::get('/posts', 'PostController@index');
+
 Route::get('/register', 'UserController@create');
 Route::post('/register', 'UserController@store');
