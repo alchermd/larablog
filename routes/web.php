36412e1f1de4b16095ecfb2d8ca2635db2279c7a
alchermd <johnalcherdoloiras@gmail.com>
2018-03-01 23:49:08 +0800

    Add route for logging users in.

--- routes/web.php
+++ routes/web.php
@@ -7,4 +7,6 @@ Route::get('/posts', 'PostController@index');
 Route::get('/register', 'UserController@create');
 Route::post('/register', 'UserController@store');
 
+Route::get('/login', 'SessionController@create');
+Route::post('/login', 'SessionController@store');
 Route::get('/logout', 'SessionController@destroy');
