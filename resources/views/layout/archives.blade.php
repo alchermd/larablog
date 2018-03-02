dc60936e933395149be5c942e7b3edcd619027ad
alchermd <johnalcherdoloiras@gmail.com>
2018-03-03 01:35:00 +0800

    Display archives dynamically.

--- resources/views/layout/archives.blade.php
+++ resources/views/layout/archives.blade.php
@@ -6,17 +6,15 @@
     <hr>
 
     <ul class="list-group text-center">
+        @foreach ($archives as $stats)
         <li class="list-group-item">
-            <a href="#">March 2018</a>
-            <span class="badge badge-pill badge-primary">3 posts</span>
+            <a href="/posts/?month={{ Carbon\Carbon::parse($stats['month'])->month }}&year={{ $stats['year'] }}">
+                {{ $stats['month'] . " " . $stats['year'] }}
+            </a>
+            <span class="badge badge-pill badge-primary">
+                {{ $stats['published'] }} post{{ $stats['published'] > 1 ? 's' : '' }}
+            </span>
         </li>
-        <li class="list-group-item">
-            <a href="#">April 2018</a>
-            <span class="badge badge-pill badge-primary">5 posts</span>
-        </li>
-        <li class="list-group-item">
-            <a href="#">May 2018</a>
-            <span class="badge badge-pill badge-primary">1 post</span>
-        </li>
+        @endforeach
     </ul>
 </div>
