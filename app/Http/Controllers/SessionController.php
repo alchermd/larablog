712f4be1fa123f3bb987b459a60ca107458c6d5f
alchermd <johnalcherdoloiras@gmail.com>
2018-03-02 13:37:40 +0800

    Register the destroy method with the guest middleware.

--- app/Http/Controllers/SessionController.php
+++ app/Http/Controllers/SessionController.php
@@ -6,6 +6,11 @@ use Illuminate\Http\Request;
 
 class SessionController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('guest')->except(['destroy']);
+    }
+
     /**
      * Display a listing of the resource.
      *
