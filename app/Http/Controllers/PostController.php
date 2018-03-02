adf2c8d3bb59f9a89ea727524f1a0f14f24a639e
alchermd <johnalcherdoloiras@gmail.com>
2018-03-02 14:14:31 +0800

    Implement show method.

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -60,7 +60,7 @@ class PostController extends Controller
      */
     public function show(Post $post)
     {
-        //
+        return view('posts.show')->with(compact('post'));
     }
 
     /**
