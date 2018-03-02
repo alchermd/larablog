15ea48cde12216690f39af20163066ff079b240e
alchermd <johnalcherdoloiras@gmail.com>
2018-03-02 14:42:13 +0800

    Add fillable fields.

--- app/Comment.php
+++ app/Comment.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Comment extends Model
 {
+    protected $fillable = ['post_id', 'body', ];
+
     public function user()
     {
         return $this->belongsTo(User::class);
