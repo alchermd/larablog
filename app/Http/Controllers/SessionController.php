90e31666a691001198435a4bdb8c222a2cd3eea4
alchermd <johnalcherdoloiras@gmail.com>
2018-03-02 15:09:52 +0800

    Add error message for invalid credentials.

--- app/Http/Controllers/SessionController.php
+++ app/Http/Controllers/SessionController.php
@@ -50,7 +50,7 @@ class SessionController extends Controller
 
         return redirect()
             ->back()
-            ->withInput();
+            ->withErrors(['Invalid credentials']);
     }
 
     /**
