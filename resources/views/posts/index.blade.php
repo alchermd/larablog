0a028404aff1ee1fb8dd27be78402b24ca464a22
alchermd <johnalcherdoloiras@gmail.com>
2018-03-03 00:59:31 +0800

    Extract the archives section to its own partial.

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -27,28 +27,7 @@
         </div>
 
         <div class="col col-md-4">
-            <div class="jumbotron">
-                <h1 class="text-center">
-                    Archives
-                </h1>
-
-                <hr>
-
-                <ul class="list-group text-center">
-                    <li class="list-group-item">
-                        <a href="#">March 2018</a>
-                        <span class="badge badge-pill badge-primary">3 posts</span>
-                    </li>
-                    <li class="list-group-item">
-                        <a href="#">April 2018</a>
-                        <span class="badge badge-pill badge-primary">5 posts</span>
-                    </li>
-                    <li class="list-group-item">
-                        <a href="#">May 2018</a>
-                        <span class="badge badge-pill badge-primary">1 post</span>
-                    </li>
-                </ul>
-            </div>
+            @include ('layout.archives')
         </div>
     </div>
 </div>
