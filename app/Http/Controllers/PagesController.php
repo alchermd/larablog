0b767b6328baf5c997d3f3bb15d1e7a9c18374ef
alchermd <johnalcherdoloiras@gmail.com>
2018-03-04 00:10:12 +0800

    Remove redirection to posts when user is logged in.

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -6,10 +6,6 @@ class PagesController extends Controller
 {
     public function home()
     {
-        if (auth()->check()) {
-            return redirect('/posts');
-        }
-
         return view('pages.home');
     }
 }
